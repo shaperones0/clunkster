44a98e98ec62870c7ea2d6f95edb90a18ada4be9
shaperones <86309764+shaperones0@users.noreply.github.com>
2026-07-05 22:48:50 +0300

    take 6

--- scripts/readme.py
+++ scripts/readme.py
@@ -49,6 +49,7 @@ Super destructive tools:
 - (TODO) Project crippler (Dev Build): replace assets with lightweight stubs for faster development
 
 Read [docs](https://shaperones0.github.io/clunkster) for more.
+
 """
 
 
